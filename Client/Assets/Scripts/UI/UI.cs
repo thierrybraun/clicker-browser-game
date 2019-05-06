c0f398b3198c8e4ec191448c44a916f76bd2dc00
Thierry Braun <thierryb123@gmail.com>
2019-05-06 23:35:45 +0200

    show time until next tick

--- Client/Assets/Scripts/UI/UI.cs
+++ Client/Assets/Scripts/UI/UI.cs
@@ -1,17 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-    public Text Food, Wood, Metal;
+    public Text Food, Wood, Metal, Time;
 
     private void Update()
     {
-        if (GameState.Instance.MyPlayer != null)
+        var State = GameState.Instance;
+        if (State.MyPlayer != null)
         {
-            Food.text = GameState.Instance.MyPlayer.Food + "";
-            Wood.text = GameState.Instance.MyPlayer.Wood + "";
-            Metal.text = GameState.Instance.MyPlayer.Metal + "";
+            Food.text = State.MyPlayer.Food + "";
+            Wood.text = State.MyPlayer.Wood + "";
+            Metal.text = State.MyPlayer.Metal + "";
+        }
+
+        if (State.LastResourceUpdate.HasValue && State.CurrentCityId.HasValue && State.TickDuration.HasValue)
+        {
+            Time.text = "Time until next tick:\n";
+            Time.text += (TimeSpan.FromSeconds(State.TickDuration.Value) - DateTime.UtcNow.Subtract(State.LastResourceUpdate.Value)).ToString(("mm':'ss"));
+        }
+        else
+        {
+            Time.text = "";
         }
     }
 }
