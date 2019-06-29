0a379d691e81488f17db294034695a10095b7ac5
Thierry Braun <thierryb123@gmail.com>
2019-06-29 23:47:12 +0200

    added icon attributions

---
+++ Client/Assets/Scripts/UI/FlexibleUI/FlexibleUIButton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class FlexibleUIButton : FlexibleUI
+    {
+        public enum ButtonType
+        {
+            Light, Dark
+        }
+
+        public ButtonType Type = ButtonType.Light;
+        private Image background;
+        private Text text;
+
+        public override void Awake()
+        {
+            background = GetComponent<Image>();
+            text = transform.Find("Text").GetComponent<Text>();
+            base.Awake();
+        }
+
+        protected override void OnSkinUI()
+        {
+            if (base.skinData)
+            {
+                switch (Type)
+                {
+                    case ButtonType.Light:
+                        text.color = base.skinData.TextDarkColor;
+                        background.color = base.skinData.ButtonBackgroundLightColor;
+                        break;
+                    case ButtonType.Dark:
+                        text.color = base.skinData.TextLightColor;
+                        background.color = base.skinData.ButtonBackgroundDarkColor;
+                        break;
+                }
+            }
+        }
+    }
+}
